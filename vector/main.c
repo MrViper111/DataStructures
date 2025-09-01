664cc3ac6cc4b7e88595d9cf5500848efd7e156e
MrViper111 <mrviper118@gmail.com>
2025-09-01 01:12:58 -0400

    Added several vector functions

---
+++ vector/main.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "vector.h"
+
+int main() {
+    vector_t *vec = vec_init(INITIAL_CAPACITY);
+
+    int is_empty = vec_empty(vec);
+    printf(is_empty ? "CORRECT! empty\n" : "WRONG! not empty\n");
+
+    printf("Adding elements...\n");
+    for (int i = 0; i < 10; i++) {
+        int *element = malloc(sizeof(int));
+        *element = i;
+        vec_push(vec, element);
+    }
+
+    printf("Last element: %d\n", *(int*)vec_back(vec));
+
+    printf("Removing last element: %d\n", *(int*)vec_pop(vec));
+    printf("Last element: %d\n", *(int*)vec_back(vec));
+
+    vector_t *vec1 = vec_init(INITIAL_CAPACITY);
+    for (int i = 0; i < 10; i++) {
+        int *element = malloc(sizeof(int));
+        *element = i + 10;
+        vec_push(vec1, element);
+    }
+
+    printf("Appending elements of vec to vec1\n");
+    vec_append(vec, vec1);
+    for (int i = 0; i < vec->size; i++) {
+        printf("%d ", *(int*)vec_at(vec, i));
+    }
+    printf("\n");
+
+    // int index = 2;
+    // int *value = malloc(sizeof(int));
+    // *value = 0;
+    // printf("Inserting into index %i...", index);
+    // vec_insert(vec, index, value);
+    // for (int i = 0; i < vec->size; i++) {
+    //     printf("%d ", *(int*)vec_at(vec, i));
+    // }
+    // printf("\n");
+
+    return 0;
+}
---
+++ vector/vector.c
@@ -0,0 +1,126 @@
+#include <stdlib.h>
+#include "vector.h"
+
+vector_t* vec_init(int capacity) {
+    vector_t *vec = malloc(sizeof(vector_t));
+    vec->elements = malloc(capacity * sizeof(void*));
+    vec->size = 0;
+    vec->capacity = capacity;
+
+    return vec;
+}
+
+void vec_increase_capacity(vector_t *vec) {
+    int capacity = vec->capacity;
+    int new_capacity = capacity * RESIZE_FACTOR;
+    capacity = new_capacity;
+
+    vec->elements = realloc(vec->elements, new_capacity * sizeof(void*));
+}
+
+int vec_empty(vector_t *vec) {
+    return vec->size == 0;
+}
+
+void* vec_at(vector_t *vec, int index) {
+    if (index < 0 || index > vec->size - 1) {
+        return NULL;
+    }
+
+    return vec->elements[index];
+}
+
+void* vec_front(vector_t *vec) {
+    return vec_at(vec, 0);
+}
+
+void* vec_back(vector_t *vec) {
+    return vec_at(vec, vec->size - 1);
+}
+
+int vec_size(vector_t *vec) {
+    return vec->size;
+}
+
+int vec_capacity(vector_t *vec) {
+    return vec->capacity;
+}
+
+void vec_reserve(vector_t *vec, int capacity) {
+    if (vec->capacity >= capacity) {
+        return;
+    }
+
+    vec->capacity = capacity;
+    vec->elements = realloc(vec->elements, vec->capacity * sizeof(void*));
+}
+
+void vec_shrink_to_fit(vector_t *vec) {
+    vec->capacity = vec->size;
+    vec->elements = realloc(vec->elements, vec->capacity * sizeof(void*));
+}
+
+void vec_clear(vector_t *vec, destructor_func_t destr_func) {
+    for (int i = 0; i < vec->size; i++) {
+        destr_func(vec->elements[i]);
+    }
+
+    vec->size = 0;
+}
+
+void vec_push(vector_t *vec, void *element) {
+    int old_capacity = vec->capacity;
+    int size = vec->size;
+
+    if (size + 1 > old_capacity) {
+        vec_increase_capacity(vec);
+    }
+
+    vec->elements[size] = element;
+    vec->size++;
+}
+
+void* vec_pop(vector_t *vec) {
+    if (vec_empty(vec)) {
+        return NULL;
+    }
+
+    void *last_element = vec_back(vec);
+    vec->size--;
+
+    return last_element;
+}
+
+void vec_append(vector_t *vec1, vector_t *vec2) {
+    vec_reserve(vec1, vec1->size + vec2->size);
+
+    for (int i = 0; i < vec2->size; i++) {
+        void *element = vec2->elements[i];
+        vec_push(vec1, element);
+    }
+
+    vec2->size = 0;
+}
+
+// void vec_insert(vector_t *vec, int index, void *element) {
+//     if (vec_empty(vec)) {
+//         vec_push(vec, element);
+//         return;
+//     }
+
+//     int size = vec->size;
+//     if (size + 1 > vec->capacity) {
+//         vec_increase_capacity(vec);
+//     }
+
+//     for (int i = size - 1; i >= index; i--) {
+//         void *current = vec_at(vec, i);
+//         void *next = vec_at(vec, i + 1);
+
+//         next = current;
+//     }
+
+//     void *new_element = vec_at(vec, index);
+//     new_element = element;
+//     vec->size++;
+// }
